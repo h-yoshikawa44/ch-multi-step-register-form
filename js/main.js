3ed45a0b8a0804a45b33b8667bb97ad04c8c492c
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2024-03-24 17:01:21 +0900

    feat: JavaScriptで複数ステップフォームの遷移を作成(#5)

--- js/main.js
+++ js/main.js
@@ -1,2 +1,49 @@
+/* global document */
+
 import '../css/reset.css';
 import '../css/style.css';
+
+const hiddenClass = 'hidden';
+const stepCurrentClass = 'step-info__step--current';
+const stepActiveClass = 'step-info__step--active';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const userInfoForm = document.getElementById('user-info-form');
+  const topicForm = document.getElementById('topic-form');
+  const summary = document.getElementById('summary');
+
+  const userInfoFormContinueButton = document.getElementById(
+    'user-info-form-continue-button',
+  );
+  const topicFormContinueButton = document.getElementById(
+    'topic-form-continue-button',
+  );
+
+  const stepInfoText = document.getElementById('step-info-text');
+  const stepOne = document.getElementById('step-one');
+  const stepTwo = document.getElementById('step-two');
+  const stepThree = document.getElementById('step-three');
+
+  const handleUserInfoNext = () => {
+    userInfoForm.classList.add(hiddenClass);
+    topicForm.classList.remove(hiddenClass);
+
+    stepInfoText.textContent = 'Step 2 of 3';
+    stepOne.classList.remove(stepCurrentClass);
+    stepOne.classList.add(stepActiveClass);
+    stepTwo.classList.add(stepCurrentClass);
+  };
+
+  const handleTopicNext = () => {
+    topicForm.classList.add(hiddenClass);
+    summary.classList.remove(hiddenClass);
+
+    stepInfoText.textContent = 'Step 3 of 3';
+    stepTwo.classList.remove(stepCurrentClass);
+    stepTwo.classList.add(stepActiveClass);
+    stepThree.classList.add(stepCurrentClass);
+  };
+
+  userInfoFormContinueButton.addEventListener('click', handleUserInfoNext);
+  topicFormContinueButton.addEventListener('click', handleTopicNext);
+});
